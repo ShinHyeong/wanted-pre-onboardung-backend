8fab7d98174b0b5733aa9ebe625d79a556140eff
ShinHyeong <limo730@naver.com>
2023-08-16 23:05:32 +0900

    add config

---
+++ src/main/java/com/psh/board/config/WebSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.psh.board.config;
+
+import com.psh.board.security.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
+
+
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // http 시큐리티 빌더
+        http.cors() // WebMvcConfig에서 이미 설정했으므로 기본 cors 설정.
+                .and()
+                .csrf()// csrf는 현재 사용하지 않으므로 disable
+                .disable()
+                .httpBasic()// token을 사용하므로 basic 인증 disable
+                .disable()
+                .sessionManagement()  // session 기반이 아님을 선언
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests() // /와 /auth/** 경로는 인증 안해도 됨.
+                .antMatchers("/", "/auth/**").permitAll()
+                .anyRequest() // /와 /auth/**이외의 모든 경로는 인증 해야됨.
+                .authenticated();
+
+        http.addFilterAfter(
+                jwtAuthenticationFilter,
+                CorsFilter.class
+        );
+
+        return http.build();
+    }
+}
