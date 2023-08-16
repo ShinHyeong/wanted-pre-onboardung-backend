6d2cdf7f35cbf55c0cc101f4d75cde10d365f5b2
ShinHyeong <limo730@naver.com>
2023-08-16 22:41:57 +0900

    add security

---
+++ src/main/java/com/psh/board/security/TokenProvider.java
@@ -0,0 +1,42 @@
+package com.psh.board.security;
+
+import com.psh.board.model.UserEntity;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Slf4j
+@Service
+public class TokenProvider {
+    private static final String SECRET_KEY = "TGlrZUlU";
+
+    public String create(UserEntity userEntity) {
+        //만료기한 : 발급시각으로부터 12시간
+        Date expiryDate = Date.from(Instant.now().plus(12, ChronoUnit.HOURS));
+
+        // JWT Token 생성
+        return Jwts.builder()
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .setSubject(userEntity.getId()) // sub
+                .setIssuer("board app") // iss
+                .setIssuedAt(new Date()) // iat
+                .setExpiration(expiryDate) // exp
+                .compact();
+    }
+
+    public String validateAndGetUserId(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+}
